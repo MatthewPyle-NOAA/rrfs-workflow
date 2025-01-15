f301e7edaeb80eebf16f9b36d5e927d1d37aa0c6
matthew pyle <Matthew.Pyle@noaa.gov>
2025-01-15 14:04:14 +0000

    Makes it hourly to 60 h, eliminates RUNLOC

--- scripts/exrrfs_awips.sh
+++ scripts/exrrfs_awips.sh
@@ -20,10 +20,10 @@ msg="RRFS post-processing for AWIPS has begun on `hostname` at `date`"
 postmsg  "$msg"
 startmsg
 
-RUNLOC=${NEST}${MODEL}
-
-for fhr in 000 003 006 009 012 015 018 021 024 027 030 033 \
-       	036 039 042 045 048 051 054 057 060 ;
+for fhr in 000 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 \
+       016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 \
+       034 035 036 037 038 039 040 041 042 043 044 045 046 047 048 \
+       049 050 051 052 053 054 055 056 057 058 059 060 ;
 do
     export FORTREPORTS="unit_vars=yes"     # Allow overriding default names.
 
